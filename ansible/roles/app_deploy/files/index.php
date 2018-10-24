b08c0ba8e493abd5b25bb08f65b8407a1365122c
Nikolay Antsiferov <nklya@hotmail.com>
2018-10-24 17:29:10 +0300

    Fibonacci app done

--- ansible/roles/app_deploy/files/index.php
+++ ansible/roles/app_deploy/files/index.php
@@ -1,1 +1,33 @@
-<?php phpinfo(); ?>
+<html>
+ <head>
+  <title>Fibonacci as a Service ツ</title>
+ </head>
+ <body>
+
+<?php
+function fibonacci($from, $to)
+{
+    $a = 0;
+    $b = 1;
+    $tmp;
+    while ($to > 0) {
+        if ($from > 0) {
+            $from--;
+        } else {
+            yield $a;
+        }
+        $tmp = $a + $b;
+        $a = $b;
+        $b = $tmp;
+        $to--;
+    }
+}
+
+foreach (fibonacci(0, htmlspecialchars($_GET["n"])) as $fib) {
+    echo $fib . " ";
+
+}
+?>
+
+ </body>
+</html>
